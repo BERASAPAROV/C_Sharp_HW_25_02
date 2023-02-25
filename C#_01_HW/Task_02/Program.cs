76e59473ba7bd05863cf7e8271ea84c54db6e671
saparovbera <saparovbera@gmail.com>
2023-02-25 12:44:01 +0600

    Task_02

---
+++ C#_01_HW/Task_02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите 2 числа и мы тебе скажем какое из них больше");
+Console.WriteLine("Введите свое первое число: ");
+string s_a = Console.ReadLine()!;
+Console.WriteLine("Введите свое первое число: ");
+string s_b = Console.ReadLine()!;
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (a > b)
+{
+    Console.WriteLine(a);    
+}
+else if (a == b)
+{
+    Console.WriteLine($"Числа {a} и {b} равны");
+}
+else
+{
+    Console.WriteLine(b);
+}
