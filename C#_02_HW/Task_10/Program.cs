b67ee79fd3927e6f10a3e152a5a26effe07ddd6a
saparovbera <saparovbera@gmail.com>
2023-03-02 20:15:47 +0600

    Task_10

---
+++ C#_02_HW/Task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Вывидите трёхзначное число и мы тебе на выходе показывает вторую цифру этого числа");
+int GetSecondDigit(int number)
+{
+    Console.WriteLine(number);
+    while(number >= 100)
+    number /= 10;
+    return number % 10;
+}
+
+int result = GetSecondDigit(new Random().Next(100, 1000));
+Console.WriteLine($"Вторая цифра этого числа -> {result}");
